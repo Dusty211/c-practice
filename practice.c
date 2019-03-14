c9ea5897e7036c6dbf936f6aa51c66ebe46a8af8
Dusty211 <kylebenz@hotmail.com>
2019-03-13 22:37:25 -0400

    got things working but not as intended

--- practice.c
+++ practice.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <math.h>
 
-int return_floats(char input);
+int return_floats(char *input);
 
 int main() {
 
@@ -18,33 +18,38 @@ int main() {
   scanf("%[^\n]%*c", str);
   printf("Your hash before: %s \n", str);
 
-  new_array = return_floats(str);
-  printf("new_array: %i \n", new_array);
-
   for(i = 0; i < strlen(str); i++ ) {
+    //char type sucks for this - only 256 values:
     output_hash[i] = output_chars[str[i] * str[i] % 9 * str[i] * str[i] % 62];
-  }
 
-  //for debugging:
-  for(i = 0; i < strlen(str); i++ ) {
+    //for debugging:
     printf("Output_hash index %i = %d \n", i, output_hash[i]);
   }
 
+  //with char types:
   printf("\nOutput_hash: %s \n", output_hash);
+
+  //function using bigger types - not working as expected
+  //not getting intended return from function
+  new_array = return_floats(str);
+  printf("new_array: %d \n", new_array);
 
   return 0;
 }
 
-int return_floats(char input) {
+int return_floats(char *input) {
 
   int i;
   int int_out[200];
-  long double for_math[200];
+  long long for_math[200];
 
   for(i = 0; i < strlen(input); i++ ) {
     for_math[i] = input[i];
+    int_out[i] = (((((for_math[i] * for_math[i] * for_math[i]) % 9) + 7) * for_math[i]) * for_math[i]) % 62;
+    //debugging
+    printf("int_out[i] %i = %d \n", i, int_out[i]);
+    // printf("for_math[i] %i = %lld \n", i, for_math[i]);
 
-    int_out[i] = for_math[i] * for_math[i] % 9 * for_math[i] * for_math[i] % 62;
-    return int_out;
   }
+  return *int_out;
 }
