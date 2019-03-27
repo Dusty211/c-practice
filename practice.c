a0a805a93b2904b964d866f730515ad943e86096
Dusty211 <kylebenz@hotmail.com>
2019-03-27 17:50:17 -0400

    working nicely. needs work with small message changes having greater effect on digest.

--- practice.c
+++ practice.c
@@ -8,7 +8,7 @@ int * return_ints(char *input);
 int main() {
 
   char str[200];
-  char output_chars[] = "0123456789abcdefjhijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char output_chars[] = "0123456789abcdefjhijklmnopqrstuvwxyz";
   int i;
   int *new_array;
 
@@ -28,15 +28,24 @@ int main() {
 int * return_ints(char *input) {
 
   int i;
-  // int int_out[200];
   int *int_out = malloc (sizeof (int) * 200);
   long long for_math[200];
+  long long element_summary = 0;
+  long long multiplier;
+
+  for(i = 0; i < strlen(input); i++) {
+    element_summary = element_summary + input[i];
+    // printf("sum for %i = %lld\n", i, element_summary);
+  }
+
+  element_summary = ((element_summary * element_summary * element_summary % 23) + 1) * 100000 % 256;
 
   for(i = 1; i < strlen(input) + 1; i++ ) {
     for_math[i] = input[i - 1];
-    int_out[i] = (((((for_math[i] * for_math[i] * for_math[i]) % 9) + 7) * for_math[i]) * for_math[i]) % 62;
+    multiplier = for_math[i] + strlen(input) + 100;
+    int_out[i] = (((((element_summary * element_summary * multiplier * multiplier) % 11) + strlen(input) + i) * multiplier) * multiplier) % 36;
     //debugging
-    printf("int_out[i] %i = %d \n", i, int_out[i]);
+    // printf("int_out[i] %i = %d \n", i, int_out[i]);
   }
   int_out[0] = strlen(input);
   printf("int_out[0] (length) = %d \n", int_out[0]);
